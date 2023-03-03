f76d95e21695e82ab80589d91debabe6c95ae4fb
Jaymin Patel <jaymin.future365@gmail.com>
2023-03-04 01:12:09 +0530

    Added Beer Repository. Closes #6

---
+++ src/main/java/dev/jaymin/msscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package dev.jaymin.msscbeerservice.repositories;
+
+import dev.jaymin.msscbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
