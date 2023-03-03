ab86e278a6b6e24763f67bf0c5e010af1bd7e964
Jaymin Patel <jaymin.future365@gmail.com>
2023-03-03 17:35:58 +0530

    Intial Porject setup. Closes #1

---
+++ src/test/java/dev/jaymin/msscbeerservice/MsscBeerServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package dev.jaymin.msscbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assert true;
+	}
+
+}
