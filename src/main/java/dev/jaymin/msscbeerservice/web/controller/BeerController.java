a83ddd94a79f97c8cff37c46f6a4b28df9742132
Jaymin Patel <jaymin.future365@gmail.com>
2023-03-03 18:04:46 +0530

    Added Web Controllers, Closes #3

---
+++ src/main/java/dev/jaymin/msscbeerservice/web/controller/BeerController.java
@@ -0,0 +1,40 @@
+package dev.jaymin.msscbeerservice.web.controller;
+
+import dev.jaymin.msscbeerservice.web.model.BeerDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/beer")
+public class BeerController {
+
+
+    @GetMapping("/{beerId}")
+    public ResponseEntity<BeerDto> handleGetBeerById(@PathVariable("BeerId") UUID beerId) {
+
+        //todo impl
+        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity handleCreateBeer(@RequestBody BeerDto beerDto) {
+
+        // todo impl
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{beerId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void handleUpdateBeer(@RequestBody BeerDto beerDto, @PathVariable("beerId") UUID beerID) {
+        // todo impl
+    }
+
+    @DeleteMapping("/{beerId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void handleDeleteBeer(@PathVariable("beerId") UUID beerId) {
+        // todo impl
+    }
+}
