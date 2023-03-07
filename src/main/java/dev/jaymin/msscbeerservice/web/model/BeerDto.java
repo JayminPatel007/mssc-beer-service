7e71a0e5f440606dc418737b6800d0c0b2b1d0aa
Jaymin Patel <jaymin.future365@gmail.com>
2023-03-07 14:51:08 +0530

    Added required Jackson Configuration annotations. Closes #13

--- src/main/java/dev/jaymin/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/dev/jaymin/msscbeerservice/web/model/BeerDto.java
@@ -1,5 +1,6 @@
 package dev.jaymin.msscbeerservice.web.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -23,8 +24,10 @@ public class BeerDto {
     @Null
     private Integer version;
     @Null
+    @JsonFormat(pattern = "yyyy-mm-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     private OffsetDateTime createdDate;
     @Null
+    @JsonFormat(pattern = "yyyy-mm-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     private OffsetDateTime lastModifiedDate;
     @NotBlank
     private String beerName;
@@ -35,6 +38,7 @@ public class BeerDto {
     private Long upc;
     @NotNull
     @Positive
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     private BigDecimal price;
 
     private Integer quantityOnHand;
