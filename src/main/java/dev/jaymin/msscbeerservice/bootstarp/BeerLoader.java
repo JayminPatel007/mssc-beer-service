73d59378541059f37c71e80e4e8f4fba2e6f8554
Jaymin Patel <jaymin.future365@gmail.com>
2023-03-13 15:44:50 +0530

    Beer bootstrap data fix

--- src/main/java/dev/jaymin/msscbeerservice/bootstarp/BeerLoader.java
+++ src/main/java/dev/jaymin/msscbeerservice/bootstarp/BeerLoader.java
@@ -15,45 +15,52 @@ import java.math.BigDecimal;
 public class BeerLoader implements CommandLineRunner {
 
     public static final String BEER_1_UPC = "0631234200036";
-    public static final String BEER_2_UPC = "0631234300013";
+    public static final String BEER_2_UPC = "0631234300019";
     public static final String BEER_3_UPC = "0083783375213";
 
     private final BeerRepository beerRepository;
 
     @Override
     public void run(String... args) throws Exception {
-        log.debug("This is called");
-        loadBeerObjects();
+        if (beerRepository.count() == 0) {
+            loadBeerObjects();
+        }
     }
 
     private void loadBeerObjects() {
         log.info("load BeerObjects has called");
-        if (beerRepository.count() == 0) {
-            beerRepository.save(Beer.builder()
-                    .beerName("Mango Bobs").beerStyle("IPA")
-                    .quantityToBrew(200)
-                    .minOnHand(12)
-                    .upc(BEER_1_UPC)
-                    .price(new BigDecimal("12.95")).build());
 
-            beerRepository.save(Beer.builder()
-                    .beerName("Galaxy Cat")
-                    .beerStyle("PALE_ALE")
-                    .quantityToBrew(200)
-                    .minOnHand(12)
-                    .upc(BEER_2_UPC)
-                    .price(new BigDecimal("11.95")).build());
+        Beer b1 = Beer.builder()
+                .beerName("Mango Bobs").beerStyle("IPA")
+                .quantityToBrew(200)
+                .minOnHand(12)
+                .upc(BEER_1_UPC)
+                .price(new BigDecimal("12.95"))
+                .build();
 
-            beerRepository.save(Beer.builder()
-                    .beerName("No Hammers On The Bar")
-                    .beerStyle("PALE_ALE")
-                    .quantityToBrew(200)
-                    .minOnHand(12)
-                    .upc(BEER_3_UPC)
-                    .price(new BigDecimal("11.95")).build());
+        Beer b2 = Beer.builder()
+                .beerName("Galaxy Cat")
+                .beerStyle("PALE_ALE")
+                .quantityToBrew(200)
+                .minOnHand(12)
+                .upc(BEER_2_UPC)
+                .price(new BigDecimal("11.95"))
+                .build();
 
-            log.info("Loaded Beers: {}", beerRepository.count());
+        Beer b3 = Beer.builder()
+                .beerName("No Hammers On The Bar")
+                .beerStyle("PALE_ALE")
+                .quantityToBrew(200)
+                .minOnHand(12)
+                .upc(BEER_3_UPC)
+                .price(new BigDecimal("11.95"))
+                .build();
 
-        }
+        beerRepository.save(b1);
+        beerRepository.save(b2);
+        beerRepository.save(b3);
+
+        log.info("Loaded Beers: {}", beerRepository.count());
+
     }
 }
